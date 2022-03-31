432d5109077160b127312ea8538c14d8b0e196af
Nischal Gurung <nizallgurung16@gmail.com>
2022-03-31 17:44:56 +0530

    Fix icon link again

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/text-utils/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button
